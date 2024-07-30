7815560ec124ef1d7b1824d5389ff4861fc88740
Piotr Sierankiewicz <piotr.sierankiewicz@rumblefish.pl>
2024-07-30 13:00:39 +0200

    update

--- runTraceTransaction.js
+++ runTraceTransaction.js
@@ -7,17 +7,11 @@ async function main() {
 
   const hardhatProvider = await hardhat.run(TASK_NODE_GET_PROVIDER, {
     chainId,
-    
   })
   console.log('hardhatProvider', hardhatProvider);
 
   const txHash = '0xe565eb3bfd815efcc82bed1eef580117f9dc3d6896db42500572c8e789c5edd4'
-  const traceResult = await hardhatProvider.send('debug_traceTransaction', [txHash], "latest",
-  {
-    disableMemory: true,
-    disableStack: true,
-    disableStorage: true,
-  },)
+  const traceResult = await hardhatProvider.send('debug_traceTransaction', [txHash])
 
   console.log(`Finished debug_traceTransaction for ${chainId}/${txHash} with ${traceResult.structLogs.length} structLogs`)
 
